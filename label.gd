02a108d365304a43bbffdf2c72b1dc18e650fbc6
Carlisle Poe <carlislepoe@gmail.com>
2026-02-02 14:22:18 -0800

    Improved how the game handles window resizing.

--- label.gd
+++ label.gd
@@ -6,11 +6,11 @@ extends Label
 @onready var current_dialog = "1"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().create_timer(8.0).timeout
+	await get_tree().create_timer(6.0).timeout
 	disclaimer.visible = false
 	label.visible = true
 	print("Ready!")
-	var tween_speed: float = text.length()*0.02
+	var tween_speed: float = text.length()*0.04
 	visible_ratio = 0.0
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_LINEAR)
@@ -33,7 +33,7 @@ func _input(event):
 		if current_dialog == "1":
 			current_dialog = "2"
 			label.text = "You want to join the resistance, yeah? Well you're gonna need to learn how to hack, so we can take down their systems.\nModern computers have a backdoor in them, that the government uses to spy on us civilians, so you'll have to settle for this very old computer. You may notice it uses a CRT display, and doesn't have a GUI...\nI'm sure it's not what you're used to, but it will work for our purposes."
-			var tween_speed: float = text.length()*0.02
+			var tween_speed: float = text.length()*0.04
 			var tween = create_tween()
 			tween.set_trans(Tween.TRANS_LINEAR)
 			tween.tween_property(label, "visible_ratio", 1.0, tween_speed)
